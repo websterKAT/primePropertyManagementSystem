3fc5228ba06dea1a1cc818ecd52eddba469eafab
nuwansomasiri8 <nuwansomasiri8@gmail.com>
2017-10-23 07:42:40 +0530

    Create property view

---
+++ application/views/propertyList_UI.php
@@ -0,0 +1,51 @@
+<div class="well">
+</div>
+	<div class="row">
+		<div class="property_item span12" style="border-style: solid; border-radius: 5px; border-width: 5px; border-color: #383838; ; padding: 5px; margin-top: 10px;">
+			<div class="span4" style="margin-left: 0px; border-style: solid; border-color: grey; border-radius: 7px;">
+				<img src="<?php echo base_url(); ?>resources/images/property1.jpg" width="500px" style="border-radius: 5px;">
+			</div> 
+			<div class="span7">
+				<u><h2 style="text-align: center;">House in Kohuwala</h2></u>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum vulputate ullamcorper. Suspendisse ultricies, libero sit amet pellentesque elementum, nisi lectus lobortis arcu, id pharetra tortor arcu quis velit. Nullam eu urna cursus, ornare dolor in, venenatis lorem. Duis orci lectus, eleifend at massa ut, faucibus dictum lectus. Nullam ante dolor, pharetra nec blandit a, luctus vitae magna. Donec condimentum ligula nunc, in hendrerit nisi vestibulum nec. Nam eu bibendum mi, sed ultricies massa. Cras sit amet sapien vitae tortor vehicula posuere. Proin eget tortor vulputate, tristique metus et, facilisis lorem.
+				</p>
+				<p>
+					<b style="font-size: 20px;">Rs: 2,500,000/=</b>
+					<a href="" style="float: right;"><button class="btn btn-info" style="height: 40px;">Read More</button></a>
+				</p>
+			</div>
+		</div>
+		<hr>
+		<div class="property_item span12" style="border-style: solid; border-radius: 5px; border-width: 5px; border-color: #383838; ; padding: 5px; margin-top: 10px;">
+			<div class="span4" style="margin-left: 0px; border-style: solid; border-color: grey; border-radius: 7px;">
+				<img src="<?php echo base_url(); ?>resources/images/property2.jpg" width="500px" style="border-radius: 5px;">
+			</div> 
+			<div class="span7">
+				<u><h2 style="text-align: center;">House in Dehiwala</h2></u>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum vulputate ullamcorper. Suspendisse ultricies, libero sit amet pellentesque elementum, nisi lectus lobortis arcu, id pharetra tortor arcu quis velit. Nullam eu urna cursus, ornare dolor in, venenatis lorem. Duis orci lectus, eleifend at massa ut, faucibus dictum lectus. Nullam ante dolor, pharetra nec blandit a, luctus vitae magna. Donec condimentum ligula nunc, in hendrerit nisi vestibulum nec. Nam eu bibendum mi, sed ultricies massa. Cras sit amet sapien vitae tortor vehicula posuere. Proin eget tortor vulputate, tristique metus et, facilisis lorem.
+				</p>
+				<p>
+					<b style="font-size: 20px;">Rs: 3,500,000/=</b>
+					<a href="" style="float: right;"><button class="btn btn-info" style="height: 40px;">Read More</button></a>
+				</p>
+			</div>
+		</div>
+		<hr>
+		<div class="property_item span12" style="border-style: solid; border-radius: 5px; border-width: 5px; border-color: #383838; ; padding: 5px; margin-top: 10px;">
+			<div class="span4" style="margin-left: 0px; border-style: solid; border-color: grey; border-radius: 7px;">
+				<img src="<?php echo base_url(); ?>resources/images/property3.jpg" width="500px" style="border-radius: 5px;">
+			</div> 
+			<div class="span7">
+				<u><h2 style="text-align: center;">House in Kollupitiya</h2></u>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dictum vulputate ullamcorper. Suspendisse ultricies, libero sit amet pellentesque elementum, nisi lectus lobortis arcu, id pharetra tortor arcu quis velit. Nullam eu urna cursus, ornare dolor in, venenatis lorem. Duis orci lectus, eleifend at massa ut, faucibus dictum lectus. Nullam ante dolor, pharetra nec blandit a, luctus vitae magna. Donec condimentum ligula nunc, in hendrerit nisi vestibulum nec. Nam eu bibendum mi, sed ultricies massa. Cras sit amet sapien vitae tortor vehicula posuere. Proin eget tortor vulputate, tristique metus et, facilisis lorem.
+				</p>
+				<p>
+					<b style="font-size: 20px;">Rs: 5,500,000/=</b>
+					<a href="" style="float: right;"><button class="btn btn-info" style="height: 40px;">Read More</button></a>
+				</p>
+			</div>
+		</div>
+	</div>
